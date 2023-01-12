c0852df8a38aae8f8b246a8383ebefa6428da50f
eGkritsis <tzenios13@gmail.com>
2023-01-12 20:32:50 +0200

    Update SpringLoginSecurityApplication.java

--- Spring-Login-Security/src/main/java/com/aueb/springloginsecurity/SpringLoginSecurityApplication.java
+++ Spring-Login-Security/src/main/java/com/aueb/springloginsecurity/SpringLoginSecurityApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLoginSecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLoginSecurityApplication.class, args);
 	}
-
 }
