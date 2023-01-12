5e8d016dc9354d62901a31dde54528786adc91c9
eGkritsis <tzenios13@gmail.com>
2023-01-12 20:34:04 +0200

    Created Logging class

---
+++ Spring-Login-Security/src/main/java/com/aueb/springloginsecurity/Logging.java
@@ -0,0 +1,46 @@
+package com.aueb.springloginsecurity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "logging")
+public class Logging {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private LocalDateTime loginTime;
+
+    public Logging() {}
+
+    public Logging(String username) {
+        this.username = username;
+        this.loginTime = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDateTime getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(LocalDateTime loginTime) {
+        this.loginTime = loginTime;
+    }
+}
