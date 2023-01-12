d17cbe146da000b0f44d70d8271c9fa01f9b6578
eGkritsis <tzenios13@gmail.com>
2023-01-12 20:32:30 +0200

    Update UserRepository.java

--- Spring-Login-Security/src/main/java/com/aueb/springloginsecurity/UserRepository.java
+++ Spring-Login-Security/src/main/java/com/aueb/springloginsecurity/UserRepository.java
@@ -1,9 +1,11 @@
 package com.aueb.springloginsecurity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByUsername(String username);
 }
