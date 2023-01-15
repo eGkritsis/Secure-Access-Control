2c3c96b5226ca9b5f5a3892d2f4805c1fb5ba16e
eGkritsis <tzenios13@gmail.com>
2023-01-15 21:35:51 +0200

    Added updateLoginAttempts function

--- Spring-Login-Security/src/main/java/com/aueb/springloginsecurity/UserRepository.java
+++ Spring-Login-Security/src/main/java/com/aueb/springloginsecurity/UserRepository.java
@@ -1,11 +1,18 @@
 package com.aueb.springloginsecurity;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<Users, Long>{
     Users findByUsername(String username);
+    public int findRemainingAttemptsByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Users u SET u.login_attempts = ?1 WHERE u.username = ?2")
+    public void updateLoginAttempts(int login_attempts, String username);
 }
